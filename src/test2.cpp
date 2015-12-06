fd0cc2996441695063c9aaa94495336b1e18f6fc
kdy <kdy@kdy-PC>
2015-12-07 04:16:02 +0900

    pyramid Structure

---
+++ src/test2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : test2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!! C++" << endl; // prints !!!Hello World!!!
+	return 0;
+}
